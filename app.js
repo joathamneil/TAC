a58de1edd3ff607d0cd5ab37dfb23f976a6dbc0d
k0ms <mark.samaniego@gmail.com>
2015-03-29 21:51:13 +0800

    sign in page and libraries

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var passport =require('passport');
+var flash = require('connect-flash');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var signin = require('./routes/signin');
+
 var app = express();
+//passport 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.use(passport.initialize());
+app.use(passport.session()); 
+app.use(flash());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,11 +33,12 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser());  
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/signin', signin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
