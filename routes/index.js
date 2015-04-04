7d26b90898efc5269363c3524544a360f9ffeced
k0ms <mark.samaniego@gmail.com>
2015-04-04 19:16:55 +0800

    Deleted unnecessary node modules added new model files and signin UI.

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,44 @@
 var express = require('express');
+var passport = require('passport');
+var Users = require('../models/users');
 var router = express.Router();
-
-//passport liberaries
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    /*
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-    */
-    var user = {
-      username: "koms",
-      password: "testabc123",
-      name: "Mark Samaniego"
-    };
-
-    if(username === "koms" && password==="testabc123") {
-      return done(null, user);
-    }
-    else {
-      return done(null, false, {message: 'Incorrect username or password!'});
-    }
-  }
-));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tanay Advunture Camp' });
 });
 
+router.get('/register', function(req, res) {
+    res.render('register', { });
+});
 
-router.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: true })
-);
+router.post('/register', function(req, res) {
+    Users.register(new Users({ username : req.body.username }), req.body.password, function(err, account) {
+        if (err) {
+            return res.render('register', { account : account });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+        });
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', { user : req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+router.get('/ping', function(req, res){
+    res.status(200).send("pong!");
+});
 
 module.exports = router;
